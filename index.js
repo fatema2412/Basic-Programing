483c4a8201d06301276a880bcfebf940a44e578b
fatema2412 <abbas.ghaniwala@smarthub.ai>
2024-11-28 17:59:02 +0530

    prime num

---
+++ index.js
@@ -0,0 +1,17 @@
+function primeNum(num){
+let count=0;
+for(let i=2;i<num;i++){
+    if(num%i==0){
+    count++
+    // return i
+    }}
+    if(count==0){
+        return true;
+    }
+
+    else {
+        return false;
+    }
+
+}
+console.log(primeNum(29));
